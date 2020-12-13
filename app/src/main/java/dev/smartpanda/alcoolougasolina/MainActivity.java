64f0e1ca9b976644626dd971df9b1e4d1cf92045
Charles Vaghetti <63205923+v4gh3tt1@users.noreply.github.com>
2020-12-13 17:52:26 -0300

    Initial Commit
    Projeto criado

---
+++ app/src/main/java/dev/smartpanda/alcoolougasolina/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.smartpanda.alcoolougasolina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
