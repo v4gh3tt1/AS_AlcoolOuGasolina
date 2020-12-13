eaf218d09c76e3768cba2ab82db7c5641d7d4929
Charles Vaghetti <63205923+v4gh3tt1@users.noreply.github.com>
2020-12-13 19:17:11 -0300

    App Melhor Opcao combustivel finalisado
    Funcionado

--- app/src/main/java/dev/smartpanda/alcoolougasolina/MainActivity.java
+++ app/src/main/java/dev/smartpanda/alcoolougasolina/MainActivity.java
@@ -3,12 +3,76 @@ package dev.smartpanda.alcoolougasolina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //criar variaveis para linkar aos intens da interface grafica
+    private TextInputEditText editPrecoAlcool, editPrecoGasolina;
+    private TextView textResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // linka as variaveis com os itens da interface grafica
+        editPrecoAlcool     = findViewById(R.id.campoAlcool);
+        editPrecoGasolina   = findViewById(R.id.campoGasolina);
+        textResultado       = findViewById((R.id.textViewResultado));
+
     }
+
+    //metodo para calcular preço acionado ao clinar no botao calcular
+
+    public void calcularPreco (View view) {
+
+        //recuperar valores digitados pelo usuario na GUI
+        String precoAlcool      = editPrecoAlcool.getText().toString();
+        String precoGasolina    = editPrecoGasolina.getText().toString();
+
+        // uso do metodo pata validar os campos digitados - não podem estar em branco
+        Boolean resValida = validarCampos(precoAlcool, precoGasolina);
+        if (resValida) {
+
+            //convertendo string para Double
+            Double valorAlcool      = Double.parseDouble( precoAlcool );
+            Double valorGasolina    = Double.parseDouble( precoGasolina );
+
+            //calculo do combustivel mais vantajoso
+
+            System.out.println(valorAlcool/valorGasolina);
+
+            if ( valorAlcool/valorGasolina <= 0.7 ){
+                textResultado.setText("Melhor abastecer com ALCOOL ;)");
+            }
+            else{
+                textResultado.setText("Melhor abastecer com GASOLINA ;)");
+            }
+        }
+
+        else {
+            textResultado.setText("Primeiro preencha os campos corretamente");
+        }
+    }
+
+
+    //metodo pata validar se os campos foram digitados
+
+    public  Boolean validarCampos (String pAlcool, String pGasolina){
+        Boolean camposValidados = true;
+
+        if ( pAlcool == null || pAlcool.equals("") ) {
+            camposValidados = false;
+        } else if ( pGasolina == null || pGasolina.equals("") ){
+            camposValidados = false;
+        }
+
+        return camposValidados;
+    }
+
 }
